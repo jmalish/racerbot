d0b1fec691c939217e1e030d2749bb407de6c7dc
jmalish <jordanmalish@gmail.com>
2015-12-14 16:56:12 -0500

    Continued work on webpage titles

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import re
 from bs4 import BeautifulSoup as Bs
 import requests
 
-message = "google.com, and http://www.twitch.tv/directory/following"
+message = "twitch.tv/racer0940 name@nasa.gov google.com words that are not links."
 
 
 def get_page_title(site):  # TODO: Make this use GETs instead of POST
@@ -13,15 +13,15 @@ def get_page_title(site):  # TODO: Make this use GETs instead of POST
     except:
         pass  # do nothing, it's probably a fake website
 
-url_regex = re.findall("[a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|de|co|tv)+", message, flags=re.IGNORECASE)
+url_regex = re.findall("(www.)?[a-zA-Z0-9\-]+\.[a-z]{2,3}", message, flags=re.IGNORECASE)
 # here, we're just seeing if the message even contains a url, not concerned with whole url yet
 if url_regex:  # if this is true, the message has a url in it
     for word in message.split():
+        word = word.strip(',').strip('.')  # get rid of trailing commas or periods (ie end of sentence)
         if "reddit" in word:
             pass  # reddit stuff is already being taken care of, no need to get it here
-        elif ("com" or "org" or "net" or "mil" or "edu" or "de" or "co" or "tv") in word:  # find 'words' with a common TLD
-            url = word.strip(',')  # get rid of any trailing commas
-            print url
-            # TODO: check for status with   requests.status_code
-        else:
-            print "!" + word  # for debugging only
+        # find 'words' with a common TLD
+        elif "." in word:
+            if "@" not in word:  # ignore emails
+                print word
+                # TODO: check for status with   requests.status_code
