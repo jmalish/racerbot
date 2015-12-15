7b20109d543d39f8d813e3c42d5cd1da76b052df
jmalish <jordanmalish@gmail.com>
2015-12-15 16:27:15 -0500

    Implement webpage title getter

--- racerbot.py
+++ racerbot.py
@@ -8,6 +8,7 @@ import fishify
 import re
 import praw
 import requests
+from requests.exceptions import ConnectionError
 from bs4 import BeautifulSoup as Bs
 
 # <editor-fold desc="Variables">
@@ -52,13 +53,24 @@ def sendmsg(message):  # function to send message, a little easier than typing i
     print "%s: I sent: %s" % (now, message)
 
 
-def get_page_title(site):  # TODO: Make this use GETs instead of POST
+def get_page_title(site):  # takes what we thinks might be a url and tries to get the page title
     try:
+        if "http" not in site:
+            site = "http://" + site  # requests needs the link to have http in front of it
+
         r = requests.get(site, headers={'user-agent': 'roboracer'})
-        html = Bs(r.text, "html.parser")
-        return html.title.text
-    except Exception:
-        pass  # do nothing, it's probably a fake website
+        if r.status_code == 200:
+            html = Bs(r.text, "html.parser")
+            return html.title.text.encode('utf-8').strip()
+        else:
+            print "Unable to reach " + site + ": " + r.status_code
+            pass  # we got a 404 page or something
+    except ConnectionError as c:
+        print "Connection Error: " + site
+        return False
+    except Exception, e:
+        print e
+        return False
 # </editor-fold desc="Basic Functions">
 # </editor-fold desc="Commands">
 
@@ -108,12 +120,21 @@ def commands(nick, channel, message):
                     sendmsg("http://www.reddit.com/r/" + subreddit_name + " - That's not a real subreddit...")
                     print e
 
-    if message.find("www.") | message.find("http://"):
+    # search for websites
+    url_regex = re.findall("(www.)?[a-zA-Z0-9\-]+\.[a-z]{2,3}", message, flags=re.IGNORECASE)
+    # here, we're just seeing if the message even contains a url, not concerned with whole url yet
+    if url_regex:  # if this is true, the message has a url in it
         for word in message.split():
-            if "http" in word:
-                sendmsg(get_page_title(word))
-            elif "www." in word:
-                sendmsg(get_page_title("http://" + word))
+            word = word.strip(',').strip('.')  # get rid of trailing commas or periods (ie end of sentence)
+            if ("reddit" in word) or ("twitch" in word):
+                # reddit and twitch stuff is already being taken care of
+                # no need to get it here
+                pass
+            elif "." in word:  # look for 'words' with a '.' in the middle
+                if "@" not in word:  # ignore emails
+                    title = get_page_title(word)
+                    if title:
+                        print title
 # </editor-fold desc="Commands">
 # <editor-fold desc="Bot">
 # setting up socket
--- test.py
+++ test.py
@@ -1,27 +1,41 @@
 import re
 from bs4 import BeautifulSoup as Bs
 import requests
+from requests.exceptions import ConnectionError
 
-message = "twitch.tv/racer0940 name@nasa.gov google.com words that are not links."
+message = "this sentence.Don't forget to visit afeadfe.com"
 
 
-def get_page_title(site):  # TODO: Make this use GETs instead of POST
+def get_page_title(site):
     try:
+        if "http" not in site:
+            site = "http://" + site  # requests needs the link to have http in front of it
+
         r = requests.get(site, headers={'user-agent': 'roboracer'})
-        html = Bs(r.text, "html.parser")
-        return html.title.text
-    except:
-        pass  # do nothing, it's probably a fake website
+        if r.status_code == 200:
+            html = Bs(r.text, "html.parser")
+            return html.title.text.encode('utf-8').strip()
+        else:
+            print "Unable to reach " + site + ": " + r.status_code
+            pass  # we got a 404 page or something
+    except ConnectionError as c:
+        print "Connection Error: " + site
+        return False
+    except Exception, e:
+        print e
+        return False
 
 url_regex = re.findall("(www.)?[a-zA-Z0-9\-]+\.[a-z]{2,3}", message, flags=re.IGNORECASE)
 # here, we're just seeing if the message even contains a url, not concerned with whole url yet
 if url_regex:  # if this is true, the message has a url in it
     for word in message.split():
         word = word.strip(',').strip('.')  # get rid of trailing commas or periods (ie end of sentence)
-        if "reddit" in word:
-            pass  # reddit stuff is already being taken care of, no need to get it here
-        # find 'words' with a common TLD
-        elif "." in word:
+        if ("reddit" in word) or ("twitch" in word):
+            # reddit and twitch stuff is already being taken care of
+            # no need to get it here
+            pass
+        elif "." in word:  # look for 'words' with a '.' in the middle
             if "@" not in word:  # ignore emails
-                print word
-                # TODO: check for status with   requests.status_code
+                title = get_page_title(word)
+                if title:
+                    print title
