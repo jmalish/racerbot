3e1a5eb19ebfddfb209dcc036cf2d92335238744
jmalish <jordanmalish@gmail.com>
2015-12-22 16:42:06 -0500

    Start work on quote system

--- readme.md
+++ readme.md
@@ -10,3 +10,5 @@ racerbot is written in Python 2.7, running it with 3.0 and up will cause issues
   - requests: Easy to use HTTP library
   - BeautifulSoup4: Translates what we get from requests into an easy to "read" format
   - cleverbot: it talks!
+  - mysql-connector-python: used for quote system (may need to use 'mysql-connector-python --allow-external mysql-connector-python')
+  
--- test.py
+++ test.py
@@ -1,60 +1,28 @@
-import requests
+from mysql.connector import connection as my_sql
 import json
-import time
-import calendar
 
-channel = "racer0940"
-api_url = "https://api.twitch.tv/kraken/streams/%s" % channel
+with open('pysecrets.json') as jsonfile:  # get contents of secrets file (contains api keys)
+    secrets = json.load(jsonfile)
 
-all_channels = ["bobross", "racer0940", "SrafaOrasp"]  # this holds all channels, on and offline
-online_channels = []  # this holds all channels that are currently live
-offline_channels = ["bobross", "racer0940", "InJayzBraiN"]  # this holds all channels that are currently offline
-timer = 120  # used to tell bot when to check for channel updates (120 = 2 minutes)
-tw_clock = 0
+sql_user = secrets["mySQLuser"]  # user for mySQL
+sql_pass = secrets["mySQLpass"]
 
 
-# used to check if it's been long enough to update streams
-def timer_check():
-    time_now = calendar.timegm(time.gmtime())
-    if (time_now - tw_clock) > timer:
-        print "Updating twitch, started streaming:"
-        now_streaming = update_stream_statuses()
-        return now_streaming
-    else:
-        return []
+try:
+    sql_connection = my_sql.MySQLConnection(user=sql_user, password=sql_pass,  # open connection to mySQL
+                                            host="jordanmalish.com", database="racerbot")
 
+    sql_cursor = sql_connection.cursor()  # cursor for mySQL
 
-def update_stream_statuses():
-    now_streaming = []  # used for channels that have started streaming
-    try:
-        # first, see if the live streams are still live
-        for channel in online_channels:
-            api_url = "https://api.twitch.tv/kraken/streams/%s" % channel
-            channel_details = requests.get(api_url)
-            # read API to see if streamer is live and put them in correct list
-            if channel_details.json()["stream"] is None:  # channel is no longer live
-                online_channels.remove(channel)  # remove the channel from list of online
-                offline_channels.append(channel)  # and move it to the offline list
-            else:
-                pass  # don't do anything, as the channel is still live
-        for channel in offline_channels:
-            api_url = "https://api.twitch.tv/kraken/streams/%s" % channel
-            channel_details = requests.get(api_url)
-            # read API to see if streamer is live and put them in correct list
-            if channel_details.json()["stream"] is not None:  # channel is now live
-                offline_channels.remove(channel)  # remove the channel from list of offline
-                online_channels.append(channel)  # and move it to the online list
-                # the channel has gone from offline to online, so we need to let the irc room know
-                now_streaming.append(channel)
-                # print "%s has started streaming" % channel  # debugging
-            else:
-                pass  # don't do anything, as the channel is still offline
-        global tw_clock
-        tw_clock = calendar.timegm(time.gmtime())
-        return now_streaming
-    except Exception, e:
-        print "Error in update_streams_status()"
-        print e
+    sql_query = "SELECT * FROM quotes"  # query to be sent to sql
+    sql_cursor.execute(sql_query)  # send query to sql
 
+    for quote in sql_cursor:  # for each response
+        quote_id = quote[0]
+        quote_user = quote[1]
+        quote_text = quote[2]
+        print "#%s - %s: %s" % (quote_id, quote_user, quote_text)
 
-print timer_check()
+    sql_connection.close()  # close connection to mySQL when done
+except Exception, e:
+    print e
