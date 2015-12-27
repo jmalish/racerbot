ecd44648d8e561accec444cd60cf12ec0bc86d68
racer0940 <jordanmalish@gmail.com>
2015-12-26 22:02:31 -0500

    Bug Fixing

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
-import twitch
-import requests
-import json
+channel = "#racerbot.testroom"
+ircmsg = ":racer0940!sid15960@gateway/web/irccloud.com/x-lxdtditmqlusbxoi PRIVMSG #racerbot.testroom :test message and : in it"
 
-# twitch.twitch_initial()
+message = ircmsg.split(' :', 1)[1]
 
-twitch.twitch_initial()
+print message
 
-print twitch.offline_channels
+# nick = ircmsg.split("!")[0].strip(":")
+# message = ircmsg.split(channel + " :")[1]
+# print "%s - %s: %s" % (now, nick, message)
+# print ircmsg
