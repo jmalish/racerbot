ee344691b1790e668f70b85b170395fc71b2af11
jmalish <jordanmalish@gmail.com>
2015-12-29 13:32:55 -0500

    Set channel and bot name to normal settings

--- racerbot.py
+++ racerbot.py
@@ -21,7 +21,7 @@ server = "irc.freenode.net"     # irc server
 port = 6667                     # irc port
 channel = "#hoggit.iracing"  # actual channel, uncomment this line when ready to join
 # channel = "#racerbot.testroom"  # test room, uncomment next line to overwrite this channel and use 'real' channel
-botnick = "racerbot_py"  # bot name
+botnick = "racerbot_py"
 ircsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # API Key variables
