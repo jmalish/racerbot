722a6a79948927297529be7b2b4e319064471806
racer0940 <jordanmalish@gmail.com>
2016-01-08 19:08:08 -0500

    Clean up log output

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
-with open("channel.log", "a") as log_file:
-    log_file.write("testafefae")
+import time
+from datetime import datetime
+
+message = "something"
+
+time_now = str(datetime.now())
+print time_now.split(".")[0]
